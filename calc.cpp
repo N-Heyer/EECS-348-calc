e150c37f138f05cb1052ae5143d2a637fa2ebc29
N-Heyer <156978640+N-Heyer@users.noreply.github.com>
2024-12-05 13:12:42 -0600

    finished the funcs to ignore spaces and make sure the eq has digits adds them to a stack including paren and handles pemdas and finishes the eq

--- calc.cpp
+++ calc.cpp
@@ -19,5 +19,49 @@ int pemdas(char op) {
 
 // creates stacks for the nums  and operators to easily store
 double evaluate(const std::string& expr) {
-    std::stack<double> numbers; 
-    std::stack<char> operators; 
+    std::stack<double> numbers;  // num stack
+    std::stack<char> operators;   // operator stack
+
+    for (int i = 0; i < expr.length(); i++) {
+        if (isspace(expr[i])) continue; // takes spaces out of account
+
+        if (isdigit(expr[i])) { // makes sure its a num
+            double num = 0;
+            while (i < expr.length() && isdigit(expr[i])) {
+                num = num * 10 + (expr[i] - '0');
+                i++;
+            }
+            numbers.push(num); // adds num to stack
+            i--; 
+        } else if (expr[i] == '(') { // handles paren
+            operators.push(expr[i]);
+        } else if (expr[i] == ')') { // handles paren
+            while (!operators.empty() && operators.top() != '(') {
+                double b = numbers.top(); numbers.pop();
+                double a = numbers.top(); numbers.pop();
+                char op = operators.top(); operators.pop();
+                numbers.push(calc(a, b, op));
+            }
+            operators.pop(); // pop the paren
+        } else { 
+            while (!operators.empty() && pemdas(operators.top()) >= pemdas(expr[i])) {
+                double b = numbers.top(); numbers.pop();
+                double a = numbers.top(); numbers.pop();
+                char op = operators.top(); operators.pop();
+                numbers.push(calc(a, b, op));
+            }
+            operators.push(expr[i]);
+        }
+    }
+
+    while (!operators.empty()) {
+        double b = numbers.top(); numbers.pop();
+        double a = numbers.top(); numbers.pop();
+        char op = operators.top(); operators.pop();
+        numbers.push(calc(a, b, op));
+    }
+
+    return numbers.top(); // answer to eq
+
+}
+
